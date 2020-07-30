a8ad26a809a6cbc352f83802bae2928af0401e50
roey-gilor <58309139+roey-gilor@users.noreply.github.com>
2020-07-30 22:17:51 +0300

    Add files via upload

---
+++ Hotel/Clients/Guest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    public class Guest
+    {
+        private int id;
+        private string name;
+        public int Id 
+        {
+            set { this.id = value; }
+            get
+            {
+                return this.id;
+            }
+        }
+        public string Name
+        {
+            set { this.name = value; }
+            get
+            {
+                return this.name;
+            }
+        }
+        public override string ToString()
+        {
+            return "id: " + this.id + " name: " + this.name;
+        }
+    }
+}
---
+++ Hotel/Hotel/FamilyRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class FamilyRoom : Room
+    {
+        public FamilyRoom(int roomNumber, float price) : base(roomNumber, price)
+        {
+
+        }
+        protected override void SetFeatures()
+        {
+            this.features = "TV, Minibar, Jacozi, couch";
+        }
+
+        protected override void SetPrice()
+        {
+            this.price = 100;
+            if (DateTime.Now.Month == 7 || DateTime.Now.Month == 8)
+            {
+                this.price += 25;
+            }
+        }
+    }
+}
