677d6bc47cab2836bd239a3cef201d30c64c02ab
roey-gilor <58309139+roey-gilor@users.noreply.github.com>
2020-07-30 22:22:34 +0300

    Add files via upload

---
+++ Hotel/Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clients;
+using Hotel;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reception.SetRooms();
+            Guest guest = new Guest();
+            guest.Id = 1;
+            guest.Name = "Roey";
+            Room room = Reception.CheckIn(guest);
+            room = (RoomForTwo)room;
+            if (Reception.CheckIn(guest) != null)
+                Console.WriteLine("Your room number is " + room.RoomNumber + 1);
+            else
+                Console.WriteLine("Sorry, all the rooms are taken");
+            Reception.SendRoomService(room);
+            Reception.CheckOut(room);
+        }
+    }
+}
